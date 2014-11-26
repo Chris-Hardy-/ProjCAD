3af4e66352d3583480956f7ab94b76059ecc33fa
Chris-Hardy- <hardy.chris.91@icloud.com>
2014-11-26 17:14:05 -0600

    Commit 26-Nov-2014

--- appSalud/ViewControllerADondeMeDirijo.h
+++ appSalud/ViewControllerADondeMeDirijo.h
@@ -8,12 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewControllerADondeMeDirijo : UIViewController
+@interface ViewControllerADondeMeDirijo : UIViewController <UIPickerViewDataSource, UIPickerViewDelegate>
 @property (weak, nonatomic) IBOutlet UIButton *botonNacido;
 @property (weak, nonatomic) IBOutlet UIButton *botonNino;
 @property (weak, nonatomic) IBOutlet UIButton *botonAdulto;
 @property (weak, nonatomic) IBOutlet UIPickerView *pickerTemperatura;
 @property (weak, nonatomic) IBOutlet UISwitch *switchHemorragia;
 @property (weak, nonatomic) IBOutlet UISwitch *switchTrasladar;
+@property (weak, nonatomic) IBOutlet UISwitch *switchPadecimientos;
 
 @end
--- appSalud/ViewControllerADondeMeDirijo.m
+++ appSalud/ViewControllerADondeMeDirijo.m
@@ -12,6 +12,7 @@
 @interface ViewControllerADondeMeDirijo (){
     bool nacido, nino, adulto, hemorragia, trasladar;
     float temp;
+    NSArray *_pickerData;
 }
 
 @end
@@ -46,6 +47,13 @@
     
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"backgroundTexture.png"]];
     
+    _pickerData = @[@"37",@"37.5",@"37.7",@"38",@"38.2",@"38.4",@"38.6",@"38.8",@"39",@"39.2",@"39.4",@"39.6",@"39.8",@"40"];
+    [self.pickerTemperatura selectRow:4 inComponent:0 animated:YES];
+    [self.pickerTemperatura reloadAllComponents];
+    
+    self.pickerTemperatura.delegate = self;
+    pickerTemperatura.dataSource = self;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -65,14 +73,52 @@
 }
 */
 - (IBAction)botonReciendNacido:(id)sender {
+    [self.botonNacido setSelected:true];
+    [self.botonAdulto setSelected:false];
+    [self.botonNino setSelected:false];
+    nacido = true;
 }
 
 - (IBAction)botonNino:(id)sender {
+    [self.botonNacido setSelected:false];
+    [self.botonAdulto setSelected:false];
+    [self.botonNino setSelected:true];
+    nino = true;
 }
 
 - (IBAction)botonAdulto:(id)sender {
+    [self.botonNacido setSelected:false];
+    [self.botonAdulto setSelected:true];
+    [self.botonNino setSelected:false];
+    adulto = true;
 }
 - (IBAction)botonEvaluar:(id)sender {
+}
+
+// The number of columns of data
+- (int)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+// The number of rows of data
+- (int)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return _pickerData.count;
+}
+
+// The data to return for the row and component (column) that's being passed in
+- (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return _pickerData[row];
+}
+
+// Catpure the picker view selection
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row
+{
+    // This method is triggered whenever the user makes a change to the picker selection.
+    // The parameter named row and component represents what was selected.
+    temp = [[_pickerData objectAtIndex:row] floatValue];
 }
 
 @end
