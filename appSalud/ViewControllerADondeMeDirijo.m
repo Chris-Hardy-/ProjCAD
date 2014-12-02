fe81647a76a4acf05da98983b7c1167c1e9a6d3d
Chris-Hardy- <hardy.chris.91@icloud.com>
2014-12-02 05:31:28 -0600

    Pre-REvision

--- appSalud/ViewControllerADondeMeDirijo.m
+++ appSalud/ViewControllerADondeMeDirijo.m
@@ -8,18 +8,28 @@
 
 #import "ViewControllerADondeMeDirijo.h"
 #import "SWRevealViewController.h"
+#import <MapKit/MapKit.h>
+#import "CustomAnnotation.h"
 
 @interface ViewControllerADondeMeDirijo (){
-    bool nacido, nino, adulto, hemorragia, trasladar;
+    bool nacido, nino, adulto, hemorragia, trasladar, quemadura, alergia, fractura;
     float temp;
     NSArray *_pickerData;
+    NSArray *_tableData;
+    NSArray *arraySucursales;
+    CustomAnnotation *tempSucursal, *sucursalCercana;
+    NSUserDefaults *userDefaults;
+    NSMutableArray *tempAnnotations;
+    CLLocationManager *locationManager;
+    CLLocationCoordinate2D ubicacionActual;
+    UIAlertView *message;
 }
 
 @end
 
 @implementation ViewControllerADondeMeDirijo
 
-@synthesize botonAdulto, botonNacido, botonNino, switchHemorragia, switchTrasladar, pickerTemperatura;
+@synthesize botonAdulto, botonNacido, botonNino, pickerTemperatura, tablePadecimientos;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -47,13 +57,54 @@
     
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"backgroundTexture.png"]];
     
-    _pickerData = @[@"37",@"37.5",@"37.7",@"38",@"38.2",@"38.4",@"38.6",@"38.8",@"39",@"39.2",@"39.4",@"39.6",@"39.8",@"40"];
-    [self.pickerTemperatura selectRow:4 inComponent:0 animated:YES];
-    [self.pickerTemperatura reloadAllComponents];
+    _pickerData = @[@"36",@"36.2",@"36.4",@"36.6",@"36.8",@"37",@"37.5",@"37.7",@"38",@"38.2",@"38.4",@"38.6",@"38.8",@"39",@"39.2",@"39.4",@"39.6",@"39.8",@"40"];
+    _tableData = @[@"Hemorragia",@"Quemadura", @"Alergias severas", @"Fractura", @"¿Se puede trasladar?"];
     
     self.pickerTemperatura.delegate = self;
-    pickerTemperatura.dataSource = self;
+    self.pickerTemperatura.dataSource = self;
+    self.tablePadecimientos.delegate = self;
+    self.tablePadecimientos.dataSource = self;
     
+    tablePadecimientos.backgroundView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"backgroundTexture.png"]];
+    
+    [self.pickerTemperatura selectRow:3 inComponent:0 animated:YES];
+    tablePadecimientos.showsVerticalScrollIndicator = YES;
+    tablePadecimientos.indicatorStyle = UIScrollViewIndicatorStyleBlack;
+    
+    [tablePadecimientos flashScrollIndicators];
+    
+    locationManager = [[CLLocationManager alloc] init];
+    
+    userDefaults =[NSUserDefaults standardUserDefaults];
+    NSData *arrayData = [userDefaults objectForKey:@"arraySucursales"];
+    arraySucursales = [NSKeyedUnarchiver unarchiveObjectWithData:arrayData];
+    tempAnnotations = [[NSMutableArray alloc] init];
+    
+    for ( int i=0; i<[arraySucursales count]; i++) {
+        tempSucursal = [arraySucursales objectAtIndex:i];
+        CLLocationCoordinate2D sucLocation = CLLocationCoordinate2DMake(tempSucursal.latitud, tempSucursal.longitud);
+        
+        [tempSucursal setCoordinate:sucLocation];
+        [tempSucursal setTitle:tempSucursal.nombre];
+        
+        [tempAnnotations addObject:tempSucursal];
+    }
+    
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    [locationManager startUpdatingLocation];
+    
+    
+    NSTimer *aTimer = [NSTimer timerWithTimeInterval:(3.0) target:self selector:@selector(timerFired:) userInfo:nil repeats:NO];
+    NSRunLoop *runner = [NSRunLoop currentRunLoop];
+    [runner addTimer:aTimer forMode: NSDefaultRunLoopMode];
+    
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    [tablePadecimientos flashScrollIndicators];
 }
 
 - (void)didReceiveMemoryWarning
@@ -62,63 +113,309 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 - (IBAction)botonReciendNacido:(id)sender {
     [self.botonNacido setSelected:true];
     [self.botonAdulto setSelected:false];
     [self.botonNino setSelected:false];
     nacido = true;
+    nino = false;
+    adulto = false;
 }
 
 - (IBAction)botonNino:(id)sender {
     [self.botonNacido setSelected:false];
     [self.botonAdulto setSelected:false];
     [self.botonNino setSelected:true];
+    nacido = false;
     nino = true;
+    adulto = false;
 }
 
 - (IBAction)botonAdulto:(id)sender {
     [self.botonNacido setSelected:false];
     [self.botonAdulto setSelected:true];
     [self.botonNino setSelected:false];
+    nacido = false;
+    nino = false;
     adulto = true;
 }
-- (IBAction)botonEvaluar:(id)sender {
-}
 
-// The number of columns of data
+
+#pragma mark PickerWheel Control
+
 - (int)numberOfComponentsInPickerView:(UIPickerView *)pickerView
 {
     return 1;
 }
 
-// The number of rows of data
 - (int)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
 {
     return _pickerData.count;
 }
 
-// The data to return for the row and component (column) that's being passed in
 - (NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
 {
     return _pickerData[row];
 }
 
-// Catpure the picker view selection
-- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
 {
-    // This method is triggered whenever the user makes a change to the picker selection.
-    // The parameter named row and component represents what was selected.
     temp = [[_pickerData objectAtIndex:row] floatValue];
+}
+
+
+#pragma mark TableView Control
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [_tableData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    //    if (cell == nil) {
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    UISwitch *switchview = [[UISwitch alloc] initWithFrame:CGRectZero];
+    cell.accessoryView = switchview;
+    [switchview addTarget:self action:@selector(updateSwitchAtIndexPath:)
+         forControlEvents:UIControlEventValueChanged];
+    [switchview setTag:indexPath.row];
+    //    }
+    cell.textLabel.text = [_tableData objectAtIndex:indexPath.row];
+    cell.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"backgroundTexture.png"]];
+    return cell;
+}
+
+- (void)updateSwitchAtIndexPath:(id)sender {
+    //    NSLog(@"%ld", (long)[sender tag]);
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:[sender tag] inSection:0];
+    UITableViewCell *cell = [self.tablePadecimientos cellForRowAtIndexPath:indexPath];
+    UISwitch *switchView = (UISwitch *)cell.accessoryView;
+    
+    if ([switchView isOn]) {
+        switch (indexPath.row) {
+            case 0:
+                hemorragia=true;
+                break;
+            case 1:
+                quemadura=true;
+                break;
+            case 2:
+                alergia=true;
+                break;
+            case 3:
+                fractura=true;
+                break;
+            case 4:
+                trasladar=true;
+                break;
+        }
+    } else {
+        switch (indexPath.row) {
+            case 0:
+                hemorragia=false;
+                break;
+            case 1:
+                quemadura=false;
+                break;
+            case 2:
+                alergia=false;
+                break;
+            case 3:
+                fractura=false;
+                break;
+            case 4:
+                trasladar=false;
+                break;
+        }
+    }
+}
+
+- (IBAction)botonEvaluar:(id)sender {
+    if (nacido && (hemorragia || fractura || quemadura || alergia)) {
+        message = [[UIAlertView alloc] initWithTitle:@"Emergencia"
+                                             message:@"Debes de llevar al recien nacido a un centro de Salud"
+                                            delegate:self
+                                   cancelButtonTitle:@"Apple Maps"
+                                   otherButtonTitles:@"Google Maps",nil];
+        [self determinarCSCercana];
+        [message show];
+    }
+    else if (nacido && temp>=38){
+        message = [[UIAlertView alloc] initWithTitle:@"Emergencia"
+                                             message:@"Debes de llevar al recien nacido a un centro de Salud"
+                                            delegate:self
+                                   cancelButtonTitle:@"Apple Maps"
+                                   otherButtonTitles:@"Google Maps",nil];
+        [self determinarCSCercana];
+        [message show];;
+    }
+    else if (nacido && temp>=36.5){
+        NSLog(@"Consejos");
+    }
+    else if (nino && temp==38){
+        [self determinarCSCercana];
+        UIAlertView *mensajeLlamar =[[UIAlertView alloc] initWithTitle:@"Precaución"
+                                                               message:[NSString stringWithFormat:@"Debes de llamar al Centro de Salud %@ para pedir consejo", sucursalCercana.nombre]
+                                                              delegate:self
+                                                     cancelButtonTitle:@"Llamar"
+                                                     otherButtonTitles:nil];
+        sucursalCercana.telefono=[sucursalCercana.telefono
+                                  stringByReplacingOccurrencesOfString:@" " withString:@""];
+        sucursalCercana.telefono=[sucursalCercana.telefono
+                                  stringByReplacingOccurrencesOfString:@"(" withString:@""];
+        sucursalCercana.telefono=[sucursalCercana.telefono
+                                  stringByReplacingOccurrencesOfString:@")" withString:@""];
+        sucursalCercana.telefono=[sucursalCercana.telefono
+                                  stringByReplacingOccurrencesOfString:@"-" withString:@""];
+        [mensajeLlamar show];
+    }
+    else if (nino && temp>=38.5){
+        [self determinarCSCercana];
+        message = [[UIAlertView alloc] initWithTitle:@"Emergencia"
+                                             message:@"Debes de llevar al niño a un centro de Salud"
+                                            delegate:self
+                                   cancelButtonTitle:@"Apple Maps"
+                                   otherButtonTitles:@"Google Maps",nil];
+        [message show];
+    }
+    else if (nino && hemorragia){
+        NSLog(@"Consejo");
+    }
+    else if (nino && ( fractura || quemadura || alergia)){
+        message = [[UIAlertView alloc] initWithTitle:@"Emergencia"
+                                             message:@"Debes de llevar al niño a un centro de Salud"
+                                            delegate:self
+                                   cancelButtonTitle:@"Apple Maps"
+                                   otherButtonTitles:@"Google Maps",nil];
+        [self determinarCSCercana];
+        [message show];
+    }
+    else if (adulto && 37.5<temp && temp<39){
+        NSLog(@"Consejos");
+    }
+    else if (adulto && temp>=39 && trasladar){
+        message = [[UIAlertView alloc] initWithTitle:@"Emergencia"
+                                             message:@"Debes de llevar al adulto a un centro de Salud"
+                                            delegate:self
+                                   cancelButtonTitle:@"Apple Maps"
+                                   otherButtonTitles:@"Google Maps",nil];
+        [self determinarCSCercana];
+        [message show];
+    }
+    else if (adulto && temp>=39 && !trasladar){
+        NSLog(@"Llamar 066");
+    }
+    else if (adulto && (hemorragia || fractura || quemadura || alergia) && trasladar){
+        message = [[UIAlertView alloc] initWithTitle:@"Emergencia"
+                                             message:@"Debes de llevar al adulto a un centro de Salud"
+                                            delegate:self
+                                   cancelButtonTitle:@"Apple Maps"
+                                   otherButtonTitles:@"Google Maps",nil];
+        [self determinarCSCercana];
+        [message show];
+    }
+    else if (adulto && (hemorragia || fractura || quemadura || alergia) && !trasladar){
+        NSLog(@"Llamar 066");
+    }
+    else{
+        NSLog(@"NADA");
+    }
+}
+
+-(void)determinarCSCercana{
+    
+    CLLocation *sucursal;
+    CLLocation *locationA = [[CLLocation alloc] initWithLatitude:ubicacionActual.latitude longitude:ubicacionActual.longitude];
+    sucursalCercana = [[CustomAnnotation alloc]init];
+    
+    //    NSMutableArray *distancias = [[NSMutableArray alloc] init];
+    CLLocationDistance distanceA = 0.0, distanceB = 0.0;
+    tempSucursal = [tempAnnotations objectAtIndex:0];
+    sucursal = [[CLLocation alloc] initWithLatitude:tempSucursal.latitud longitude:tempSucursal.longitud];
+    distanceA  = [locationA distanceFromLocation:sucursal];
+    sucursalCercana = tempSucursal;
+    
+    for (int i=1; i<tempAnnotations.count; i++) {
+        tempSucursal = [tempAnnotations objectAtIndex:i];
+        
+        if ([tempSucursal.tipo isEqualToString:@"Centro de Salud"]) {
+            sucursal = [[CLLocation alloc] initWithLatitude:tempSucursal.latitud longitude:tempSucursal.longitud];
+            distanceB = [locationA distanceFromLocation:sucursal];
+            
+            if (distanceB<distanceA) {
+                distanceA=distanceB;
+                sucursalCercana = tempSucursal;
+            }
+            
+        }
+        
+    }
+    
+    
+}
+
+#pragma mark - CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"didFailWithError: %@", error);
+    UIAlertView *errorAlert = [[UIAlertView alloc]
+                               initWithTitle:@"Error" message:@"Failed to Get Your Location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [errorAlert show];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    NSLog(@"didUpdateToLocation: %@", newLocation);
+    CLLocation *currentLocation = newLocation;
+    
+    if (currentLocation != nil) {
+        ubicacionActual = newLocation.coordinate;
+    }
+}
+
+-(void)timerFired:(NSTimer *) theTimer
+{
+    [locationManager stopUpdatingLocation];
+}
+
+
+- (void) alertView:(UIAlertView *)alertView willDismissWithButtonIndex:(NSInteger)buttonIndex {
+    if ([alertView.title isEqualToString:@"Emergencia"]) {
+        CLLocationCoordinate2D rdOfficeLocation = CLLocationCoordinate2DMake(sucursalCercana.latitud, sucursalCercana.longitud);
+        if (buttonIndex==0) {
+            //Apple Maps, using the MKMapItem class
+            MKPlacemark *placemark = [[MKPlacemark alloc] initWithCoordinate:rdOfficeLocation addressDictionary:nil];
+            MKMapItem *item = [[MKMapItem alloc] initWithPlacemark:placemark];
+            item.name = sucursalCercana.nombre;
+            [item openInMapsWithLaunchOptions:nil];
+        } else if (buttonIndex==1) {
+            //Google Maps
+            //construct a URL using the comgooglemaps schema
+            NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"comgooglemaps://?center=%f,%f",rdOfficeLocation.latitude,rdOfficeLocation.longitude]];
+            if (![[UIApplication sharedApplication] canOpenURL:url]) {
+                UIAlertView *messageError = [[UIAlertView alloc] initWithTitle:@"Error"
+                                                                       message:@"No se encontro la app Google Maps instalada."
+                                                                      delegate:self
+                                                             cancelButtonTitle:@"Ok"
+                                                             otherButtonTitles:nil];
+                [messageError show];
+                
+                //left as an exercise for the reader: open the Google Maps mobile website instead!
+                
+            } else {
+                [[UIApplication sharedApplication] openURL:url];
+            }
+        }
+    }
+    else if ([alertView.title isEqualToString:@"Precaución"]){
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:sucursalCercana.telefono]];
+    }
 }
 
 @end
